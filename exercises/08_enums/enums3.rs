0ac5d2ba4738b6afaaccf2c189a868e100d1ce62
Bryan351018 <32482222+Bryan351018@users.noreply.github.com>
2026-05-23 23:57:30 -0400

    Finished exercises 6-9

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -44,8 +44,24 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: Create a match expression to process the different message
-        // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => {
+                self.width = width;
+                self.height = height;
+            }
+            Message::ChangeColor(r, g, b) => {
+                self.color = (r, g, b);
+            }
+            Message::Echo(s) => {
+                self.message = s;
+            }
+            Message::Move(p) => {
+                self.position = p;
+            }
+            Message::Quit => {
+                self.quit = true
+            }
+        }
     }
 }
 
